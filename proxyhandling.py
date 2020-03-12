faa43f390e7985a2ada42274284d4a6eee72305d
alexpilk <alex.pilkevych@gmail.com>
2020-03-12 15:15:06 +0100

    Extracted methods for proxyhandling

--- proxyhandling.py
+++ proxyhandling.py
@@ -1,10 +1,8 @@
-import requests
-import re, bs4, math
-from joblib import Parallel, delayed
 import random
-from pymongo import InsertOne, DeleteOne, ReplaceOne
-import pymongo
+
 import numpy as np
+import pymongo
+from pymongo import ReplaceOne
 
 
 class FailedAfterRetries(Exception):
@@ -12,59 +10,67 @@ class FailedAfterRetries(Exception):
 
 
 class DBProxyHandler:
-    db = None
-    proxies = []
 
     def __init__(self, db):
         self.db = db
 
-    def upload(self, proxyList):
-        cleanedList = [item.replace("\n", "") for item in proxyList]
+    def upload(self, proxies):
+        requests = [
+            ReplaceOne({"address": item}, {"address": item, "successful_job_completion": 0}, upsert=True) for
+            item in
+            proxies]
         self.db.proxies.bulk_write(
-            [ReplaceOne({"address": item}, {"address": item, "successful_job_completion": 0}, upsert=True) for item in
-             cleanedList],
+            requests,
             ordered=False)
 
-    def pick(self, n=1, nTries=3):
+    def pick(self, n=1, retries=3):
         if n < 1:
             raise ValueError("you must at least one proxy")
 
-        if nTries < 3:
+        if retries < 3:
             raise FailedAfterRetries
 
         try:
-            # strategy: proxys that work well shoul be reused more often. Proxies that didn't work for 45 consecutive requests are dropped
-            proxies = list(self.db.proxies.find({"successful_job_completion": {"$gt": -30}}).limit(min(10000, 1000 * n)))
-            print(proxies)
-            for proxy in proxies:
-                proxy["score"] = random.random() * min(max(proxy["successful_job_completion"], -5), 5) + 5
-
-            # chosenProxies = random.choices(population=proxies, weights=[proxy["score"] for proxy in proxies], k=n)
-            scores = [proxy["score"] for proxy in proxies]
-            score_sum = np.sum(scores)
-            p = np.array(scores)
-            p /= score_sum
-            chosenProxies = np.random.choice(proxies, min(n, len(proxies)), replace=False, p=p if score_sum != 0 else None)
-
-            if len(proxies) == 0:
+            proxies = self._filter_active_proxies(n)
+            addresses = [proxy["address"] for proxy in proxies]
+            scores = [proxy["successful_job_completion"] for proxy in proxies]
+            scores = self._calculate_scores(scores)
+            p = self._calculate_probabilities(scores)
+            chosen_proxies = np.random.choice(addresses, min(n, len(proxies)), replace=False,
+                                              p=p)
+            if not proxies:
                 raise ValueError("no proxies available!")
-            else:
-                if n == 1:
-                    return dict(chosenProxies[0])["address"]
-                else:
-                    return [dict(pro)["address"] for pro in chosenProxies]
+            return chosen_proxies[0] if n == 1 else chosen_proxies
         except pymongo.errors.AutoReconnect:
             print("pymongo error in proxy.pick: could not autoreconnect")
-            self.pick(n, nTries-1)
+            self.pick(n, retries - 1)
 
-    def feedback(self, address, counter=1, nTries=3):
-        if nTries < 0:
+    def _filter_active_proxies(self, n):
+        """
+        Drops proxies that have not responded after 30 requests.
+        """
+        return list(self.db.proxies.find({"successful_job_completion": {"$gt": -30}}).limit(min(10000, 1000 * n)))
+
+    def _calculate_scores(self, proxies):
+        def translate_score(score):
+            return min(max(score, -5), 5) + 5
+
+        return [random.random() * translate_score(score) for score in proxies]
+
+    def _calculate_probabilities(self, scores):
+        score_sum = np.sum(scores)
+        return np.array(scores) / score_sum if score_sum else None
+
+    def feedback(self, address, counter=1, retries=3):
+        if retries < 0:
             raise FailedAfterRetries
 
         try:
             proxy = self.db.proxies.find_one({"address": address})
+            new_score = proxy.get("successful_job_completion", 0) + counter if proxy else counter
             self.db.proxies.update_one({"address": address}, {
-                "$set": {"successful_job_completion": (proxy.get("successful_job_completion", 0) + counter if proxy is not None else counter)}}, upsert=True)  # allow max 15 plus points. if proxy goes offline, max 45 req will drop it
+                "$set": {"successful_job_completion": new_score}},
+                                       upsert=True)
         except pymongo.errors.AutoReconnect:
             print("pymongo error in feedback: could not autoreconnect")
-            self.feedback(address, counter, nTries-1)
+            self.feedback(address, counter, retries - 1)
